5ad47b605619c16ce4c977b8209cd8c19b832b82
xanshengna <1637337019@qq.com>
2021-07-14 20:51:32 +0800

    完成了主页功能的开发

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -1,4 +1,10 @@
 //注意每次调用$.get/ajax/post之前都会调用ajaxPrefliter这个函数，在函数中我们可以拿到Ajax提供的配置对象
 $.ajaxPrefilter(function(options) {
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+        //统一为有权限的接口，设置headers请求头
+        // if (options.url.indexOf('/my') !== -1) {
+        //     options.headers = {
+        //         Authorization: localStorage.getItem('token') || ''
+        //     }
+        // }
 })
